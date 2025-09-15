4559fd1c23c3049c4c99ec7b60924d9a456e023f
Molidier <azhimukhanbetmoldir@gmail.com>
2025-09-15 18:34:28 +0500

    for new revision

--- test/test.py
+++ test/test.py
@@ -1,6 +1,6 @@
 # SPDX-FileCopyrightText: © 2024 Tiny Tapeout
 # SPDX-License-Identifier: Apache-2.0
-#XD
+#XDXDXD
 import cocotb
 from cocotb.clock import Clock
 from cocotb.triggers import ClockCycles
